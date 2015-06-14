5ea476c5ffb7ce8f12602d75339c39169322e21e
hbrunet <hectorbrunet@gmail.com>
2015-06-13 21:31:53 -0300

    Despliegue DB en Heroku

--- models/models.js
+++ models/models.js
@@ -1,13 +1,29 @@
 var path = require('path');
 
+// Postgres DATABASE_URL = postgres://user:passwd@host:port/database
+// SQLite   DATABASE_URL = sqlite://:@:/
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name = (url[6] || null);
+var user = (url[2] || null);
+var pwd = (url[3] || null);
+var protocol = (url[1] || null);
+var dialect = (url[1] || null);
+var port = (url[5] || null);
+var host = (url[4] || null);
+var storage = process.env.DATABASE_STORAGE;
+
 // Cargar Modelo ORM
 var Sequelize = require('sequelize');
 
 // Usar BBDD SQLite o Postgres
-var sequelize = new Sequelize(null, null, null,
+var sequelize = new Sequelize(DB_name, user, pwd,
     {
-        dialect: "sqlite",
-        storage: "quiz.sqlite"
+        dialect: protocol,
+        protocol: protocol,
+        port: port,
+        host: host,
+        storage: storage,  // solo SQLite (.env)
+        omitNull: true      // solo Postgres
     }
 );
 
@@ -19,7 +35,7 @@ exports.Quiz = Quiz;
 sequelize.sync().success(function () {
     // success(..) ejecuta el manejador una vez creada la tabla
     Quiz.count().success(function (count) {
-        if (count === 0) { // la tabla se inicializa solo si está vacía
+        if (count === 0) { // la tabla se inicializa solo si estï¿½ vacï¿½a
             Quiz.create(
                 {pregunta: 'Capital de Italia', respuesta: 'Roma'}
             ).success(function () {

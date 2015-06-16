2abdfde8838c5ba5213eedaed12f2083ddc09f1a
hbrunet <hectorbrunet@gmail.com>
2015-06-16 17:37:32 -0300

    Autoload

--- routes/index.js
+++ routes/index.js
@@ -3,18 +3,21 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz' });
+// Página de entrada (home page)
+router.get('/', function (req, res) {
+    res.render('index', {title: 'Quiz'});
 });
 
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load);  // autoload :quizId
+
 // Definición de rutas de /quizes
-router.get('/quizes',                      quizController.index);
-router.get('/quizes/:quizId(\\d+)',        quizController.show);
+router.get('/quizes', quizController.index);
+router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
-router.get('/author', function(req, res) {
-  res.render('author', { title: 'Quiz' });
+router.get('/author', function (req, res) {
+    res.render('author', {title: 'Quiz'});
 });
 
 module.exports = router;

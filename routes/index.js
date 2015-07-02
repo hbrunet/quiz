1a2e7292b88279516159b076208ffb9f376212cf
hbrunet <hectorbrunet@gmail.com>
2015-07-02 16:53:04 -0300

    Autenticación y sesión

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller');
 
 // Página de entrada (home page)
 router.get('/', function (req, res) {
@@ -11,6 +12,11 @@ router.get('/', function (req, res) {
 
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load);  // autoload :quizId
+
+// Definición de rutas de sesion
+router.get('/login', sessionController.new);     // formulario login
+router.post('/login', sessionController.create);  // crear sesión
+router.get('/logout', sessionController.destroy); // destruir sesión
 
 // Definición de rutas de /quizes
 router.get('/quizes', quizController.index);
@@ -28,6 +34,6 @@ router.get('/author', function (req, res) {
 
 // Definición de rutas de comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
-router.post('/quizes/:quizId(\\d+)/comments',    commentController.create);
+router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
 
 module.exports = router;
